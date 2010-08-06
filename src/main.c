1817186c2e2abfc1eb80e84d5a1ab9bb1e6b12d0
dmitry <kondra2lp@gmail.com>
2010-08-06 23:46:17 +0400

    remove comments

--- src/main.c
+++ src/main.c
@@ -8,8 +8,6 @@ int main()
 {
 		Data *data = data_new (2000, 1000000, 100);
 		process_log (data, "../cleo-short.log");
-		//print_queue_tasks (data, "regular");
-		//print_queues (data->aqueue);
 
 		return 0;
 }
