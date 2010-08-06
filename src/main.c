92448fb806939273a048151165019d2c3a8c4578
dmitry <kondra2lp@gmail.com>
2010-08-07 00:03:38 +0400

    debugging printf

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <time.h>
+#include <stdio.h>
 
 #include "array.h"
 #include "hash.h"
@@ -8,6 +9,8 @@ int main()
 {
 		Data *data = data_new (2000, 1000000, 100);
 		process_log (data, "../cleo-short.log");
+
+    printf ("log processed\n");
 
 		return 0;
 }
