a4408fe9c2f6c71e7efe2732dc24feaeaa39606a
dmitry <kondra2lp@gmail.com>
2010-08-08 05:00:28 +0400

    deleted tasks count, succeded counts removed
    This is strange, but many tasks returns 1
    even if there is END_TASK0 event in log.
    I don't know if it is success or not,
    so I removed this counters from output.
    I also added deleted task counter for user
    and P_DELETED option for output mask.
    I've made print_user and print_queue global,
    so now they can be used in main.c.

--- src/process.c
+++ src/process.c
@@ -15,8 +15,6 @@ static User* user_new (const char *name);
 static Queue* queue_new (const char *name);
 static Task* task_new (const char *user, const char *queue);
 static void print_task (Task *cur_task, int mask);
-static void print_queue (Queue *cur_queue, int mask);
-static void print_user (User *cur_user, int mask);
 
 Data* data_new (int max_user, int max_task, int max_queue)
 {
@@ -97,6 +95,7 @@ static Task* task_new (const char *user, const char *queue)
     task->np = 0;
     task->np_extra = 0;
     task->sig = -1;
+    task->status = 0;
     task->queue_add_time = -1;
     task->run_time = -1;
     task->total_run_time = -1;
@@ -212,10 +211,8 @@ int process_log (Data *data, const char *filename)
                     break;
                 //it means that task was runned before log file begins
                 if (cur_task->run_time == -1)
-                {
                     cur_task->deleted = 1;
-                    break;
-                }
+
                 cur_task->sig = entry->end_task_act.sig;
                 cur_task->status = entry->end_task_act.status;
                 cur_task->total_run_time = entry->tm - cur_task->run_time;
@@ -233,6 +230,8 @@ int process_log (Data *data, const char *filename)
 
                 if (cur_task->sig != 0)
                     cur_user->killed++;
+                if (cur_task->deleted == 1)
+                    cur_user->deleted++;
 
                 cur_user->total_time += cur_task->total_run_time;
                 cur_user->np += cur_task->np;
@@ -299,6 +298,7 @@ static void print_task (Task *cur_task, int mask)
         printf ("NP_extra: %d\n", cur_task->np_extra);
     if (mask & P_SIGNAL)
         printf ("Signal: %d\n", cur_task->sig);
+        printf ("Status: %d\n", cur_task->status);
     if (mask & P_ADD_TIME)
         printf ("Add time: %ld\n", cur_task->queue_add_time);
     if (mask & P_BEGIN_TIME)
@@ -329,7 +329,7 @@ void print_tasks (Array *atask, int mask)
         print_task (atask->data[i], mask);
 }
 
-static void print_user (User *cur_user, int mask)
+void print_user (User *cur_user, int mask)
 {
     if (cur_user == NULL)
         return;
@@ -356,12 +356,16 @@ static void print_user (User *cur_user, int mask)
 //        printf ("NP: %d\n", cur_user->np);
 //    if (mask & P_NP_EXTRA)
 //        printf ("NP_extra: %d\n", cur_user->np_extra);
+    if (mask & P_TOTAL_TASK)
+        printf ("Task count: %d\n", cur_user->task->len);
     if (mask & P_KILLED)
         printf ("killed tasks: %d\n", cur_user->killed);
-    if (mask & P_SUCCEDED)
-        printf ("succeded tasks: %d\n", cur_user->succeded);
-    if (mask & P_UNSUCCEDED)
-        printf ("unsucceded tasks: %d\n", cur_user->unsucceded);
+//    if (mask & P_SUCCEDED)
+//        printf ("succeded tasks: %d\n", cur_user->succeded);
+//    if (mask & P_UNSUCCEDED)
+//        printf ("unsucceded tasks: %d\n", cur_user->unsucceded);
+//    if (mask & P_DELETED)
+//        printf ("Deleted task count: %d\n", cur_user->deleted);
 
     if (mask != 0)
         printf ("\n");
@@ -379,13 +383,15 @@ void print_users (Array *auser, int mask)
         print_user (auser->data[i], mask);
 }
 
-static void print_queue (Queue *cur_queue, int mask)
+void print_queue (Queue *cur_queue, int mask)
 {
     if (cur_queue == NULL)
         return;
 
     if (mask & P_QUEUE_NAME)
         printf ("Name: %s\n", cur_queue->name);
+    if (mask & P_TOTAL_TASK)
+        printf ("Task count: %d\n", cur_queue->task->len);
 //    if (mask & P_TOTAL_TIME)
 //    {
 //        long long hh = cur_queue->total_time / 3600;
@@ -414,150 +420,3 @@ void print_queues (Array *aqueue, int mask)
         print_queue (aqueue->data[i], mask);
 }
 
-Array* task_time_filter (Array *task, int b_time, int e_time)
-{
-    if (task == NULL)
-        return NULL;
-
-    Array *res = array_new (0);
-    Task *cur_task;
-
-    int i;
-
-    for (i = 0; i < task->len; i++)
-    {
-        cur_task = (Task*)task->data[i];
-        if (cur_task->run_time >= b_time && cur_task->run_time + cur_task->total_run_time <= e_time)
-            array_append (res, cur_task);
-    }
-
-    return res;
-}
-
-Array* task_run_time_filter (Array *task, int min_time, int max_time)
-{
-    Array *res = array_new (0);
-    Task *cur_task;
-
-    int i;
-
-    for (i = 0; i < task->len; i++)
-    {
-        cur_task = (Task*)task->data[i];
-        if (cur_task->total_run_time >= min_time && cur_task->total_run_time <= max_time)
-            array_append (res, cur_task);
-    }
-
-    return res;
-}
-
-Array* task_queue_filter (Array *task, Array *queue)
-{
-    Array *res = array_new (0);
-
-    Task *cur_task;
-
-    int i, j;
-    char *q_name;
-
-    for (i = 0; i < task->len; i++)
-    {
-        cur_task = (Task*) task->data[i];
-        q_name = cur_task->queue;
-        for (j = 0; j < queue->len; j++)
-            if (strcmp (q_name, queue->data[j]) == 0)
-            {
-                array_append (res, cur_task);
-                break;
-            }
-    }
-
-    return res;
-}
-
-Array* task_np_filter (Array *task, int min_np, int max_np)
-{
-    Array *res = array_new (0);
-    Task *cur_task;
-
-    int i;
-
-    for (i = 0; i < task->len; i++)
-    {
-        cur_task = (Task*)task->data[i];
-        if (cur_task->np >= min_np && cur_task->np <= max_np)
-            array_append (res, cur_task);
-    }
-
-    return res;
-}
-
-Array* user_total_time_filter (Array *users, long long min_time, long long max_time)
-{
-    Array *res = array_new (0);
-    User *cur_user;
-
-    int i;
-
-    for (i = 0; i < users->len; i++)
-    {
-        cur_user = (User*)users->data[i];
-        if (cur_user->total_time >= min_time && cur_user->total_time <= max_time)
-            array_append (res, cur_user);
-    }
-
-    return res;
-}
-
-Array* user_cpu_hours_filter (Array *users, long long min_time, long long max_time)
-{
-    Array *res = array_new (0);
-    User *cur_user;
-
-    int i;
-
-    for (i = 0; i < users->len; i++)
-    {
-        cur_user = (User*)users->data[i];
-        if (cur_user->cpu_hours >= min_time && cur_user->cpu_hours <= max_time)
-            array_append (res, cur_user);
-    }
-
-    return res;
-}
-
-Array* queue_total_time_filter (Array *queues, long long min_time, long long max_time)
-{
-    Array *res = array_new (0);
-
-    Queue *cur_queue;
-
-    int i;
-
-    for (i = 0; i < queues->len; i++)
-    {
-        cur_queue = (Queue*) queues->data[i];
-        if (cur_queue->total_time >= min_time && cur_queue->total_time <= max_time)
-            array_append (res, cur_queue);
-    }
-
-
-    return res;
-}
-
-Array* queue_cpu_hours_filter (Array *queues, long long min_time, long long max_time)
-{
-    Array *res = array_new (0);
-    Queue *cur_queue;
-
-    int i;
-
-    for (i = 0; i < queues->len; i++)
-    {
-        cur_queue = (Queue*)queues->data[i];
-        if (cur_queue->cpu_hours >= min_time && cur_queue->cpu_hours <= max_time)
-            array_append (res, cur_queue);
-    }
-
-    return res;
-}
